fd37741a777ab541db99dfee9084547b2f04b282
Shenghan Mei <72514606+Shenghan96@users.noreply.github.com>
2020-10-09 15:11:04 +0800

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -6,3 +6,5 @@ but for multiples of 3 print "fizz" instead of the number,
 for multiples of 5 print "buzz" instead of the number,
 and for multiples of both 3 and 5 print "fizzbuzz" instead of the number.
 """
+for i in range(1, 101):
+  print('fizz'[i%3*len('fizz')::] + 'buzz'[i%5*len('buzz')::] or i)
