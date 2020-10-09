a9357045d1b4b9811dce862fc4e57afc8fb5b7e1
Shenghan Mei <72514606+Shenghan96@users.noreply.github.com>
2020-10-09 15:37:17 +0800

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -10,12 +10,33 @@ import time # to compute runtimes
 from tqdm import tqdm # progress bar
 
 # Question 2
-def fibonacci_recursive(n):
+def fiboncci_recursive(n):
+    if n <= 1:
+        return n
+    else:
+        return(fiboncci_recursive(n-1) + fiboncci_recursive(n-2))
+
+for i in range(30):
+    print(fiboncci_recursive(i))
+
     pass
 
 
 # Question 2
 def fibonacci_iter(n):
+    a=0;
+    b=1;
+    i=1;
+    yield a
+    while i<=n:
+        yield b
+        a,b=b,a+b;
+        i=i+1;
+    
+
+for x in fibonacci_iter(29):
+    print(x)
+
     pass
 
 
